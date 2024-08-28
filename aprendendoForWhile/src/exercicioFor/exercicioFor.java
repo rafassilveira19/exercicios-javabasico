0295d61e64e32e546f307705121d0c3796259225
Rafael Silveira <rafassilveira19@gmail.com>
2024-08-28 17:27:45 -0300

    exercícios de fixação For. Desenvolvendo lógica de programção.

---
+++ aprendendoForWhile/src/exercicioFor/exercicioFor.java
@@ -0,0 +1,16 @@
+package exercicioFor;
+public class exercicioFor {
+    public static void main(String[] args) throws Exception {
+        
+    //Escreva um programa que imprima os números de 1 a 20 usando um loop for
+        
+        
+    for (int i = 1; i <= 20; i++) {
+        
+        
+    System.out.println(i);
+
+        }
+    }
+}
+ 
---
+++ aprendendoForWhile/src/exercicioFor2/exercicioFor2.java
@@ -0,0 +1,19 @@
+package exercicioFor2;
+
+public class exercicioFor2 {
+    public static void main(String[] args) throws Exception {
+        
+    //Escreva um programa que calcule a soma dos números de 1 a 50 usando um loop for e imprima o resultado.
+        
+    int soma = 0;
+        
+    for (int i = 1; i <= 50; i++) {
+        
+        soma += i;
+
+    System.out.println(soma);
+
+        }
+    }
+}
+ 
